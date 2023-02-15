7188e3945fa3e5c285c19766b6642f3025c0524e
余国雄 <13500283267@139.com>
2023-02-15 23:29:11 +0800

    second commit

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+namespace Custmsg
+{
+    public class Class1
+    {
+        int i;
+        int j;
+        int z;
+    }
+}
